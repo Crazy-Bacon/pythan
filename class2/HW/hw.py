a65bcb9cf99613ded35d522631561cb0bc7419cf
unknown <euan012chen@gmail.com>
2022-09-21 17:33:14 +0800

    HW2

---
+++ class2/HW/hw.py
@@ -0,0 +1,22 @@
+'''
+apple = 1000
+apple = 12
+minor = False
+name = "David"
+weight = 64.5
+zip = "545"
+
+int  :apple apple
+bool :minor
+str  :name zip
+float:weight
+
+example:
+ball = 100
+talk = 'hello'
+則要在型態列表中填入該變數
+int:ball
+str:talk
+
+'''
+''''''
--- class2/class/20220914.py
+++ class2/class/20220914.py
@@ -1,4 +1,13 @@
-from ntpath import join
+#max()
+#len()
+#字串'heoll'"heoll
+# 字串可以+-*
+#true False
+# 整數、小數(浮點數)
+# None
+# 數字運算:+、-、*、/、//、%、**
+# a = 1 把右邊存到左邊
 
-
-join
+# git add .
+# git commit -m "提示文字"
+# git push
