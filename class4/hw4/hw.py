b1be63ec2f568471f7fe89fa495ce989991adb0f
unknown <euan012chen@gmail.com>
2022-10-12 19:04:19 +0800

    class5

--- class4/hw4/hw.py
+++ class4/hw4/hw.py
@@ -7,5 +7,5 @@ else:
     c = 5 / 9 * (f - 32)
     print("華氏溫度" + str(f) + "等於華氏溫度" + str(c) + "C")
 # git add .
-# git commit -m "提示文字"
+# git commit -m "class4"
 # git push
---
+++ class5/class/class5.py
@@ -0,0 +1,63 @@
+# == 相等 1 == 1 True
+# != 不相等 1 != 0 True
+# >= 大於等於 1 >= 2 False
+# <= 小於等於 1 <= 2 True
+# > 大於 1 > 2 False
+# < 小於 1 < 2 True
+# >>> 1 == 1
+# True
+# >>> 1 == 1.0
+# True
+# >>> 1 == "1"
+# False
+#
+# >>> not False
+# True
+# >>> not True
+# False
+# >>> not 1 == 1
+# False
+# >>> not not False
+# False
+# >>> True and True
+# True
+# >>> True and False
+# False
+# >>> False and True
+# False
+# >>> False and False
+# False
+# >>>
+# >>> True or True
+# True
+# >>> True or False
+# True
+# >>> False or True
+# True
+# >>> False or False
+# False
+# >>>
+
+# password = input("請輸入密碼:")
+
+# if password == "0912":
+#     print("歡迎光臨")
+# elif password == "99999":
+#     print("歡迎hi")
+# else:
+#     print("密碼錯誤")
+# score = float(input("Please input score: "))
+# if score >= 90:
+#     grade = "A"
+# elif score >= 80:
+#     grade = "B"
+# elif score >= 70:
+#     grade = "C"
+# elif score >= 60:
+#     grade = "D"
+# else:
+#     grade = "E"
+# print("Your grade is:", grade)
+# git add .
+# git commit -m "class5"
+# git push
