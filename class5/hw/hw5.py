938ca8ad5a446ef15979958329261c9c8353494b
unknown <euan012chen@gmail.com>
2022-10-19 17:46:18 +0800

    hw5

--- class5/hw/hw5.py
+++ class5/hw/hw5.py
@@ -25,3 +25,10 @@ w = float(input("請輸入體重"))
 
 bmi = w / h**2
 print("你的bmi" + str(bmi))
+
+if bmi >= 14.8 and bmi <= 20.7:
+    print('體重正常')
+elif bmi < 14.8:
+    print("體重過輕")
+else:
+    print("體重過重")
